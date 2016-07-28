062a71cf59005dac727bdce23ed0c9f993209311
Stacia Hoover <staciaray@roadrunner.com>
2016-07-28 16:59:14 +0000

    added Dict. contents

--- ex39_test.rb
+++ ex39_test.rb
@@ -17,3 +17,37 @@ Dict.set(cities, 'NY', 'New York')
 Dict.set(cities, 'OR', 'Portland')
 
 
+# puts out some cities
+puts '-' * 10
+puts "NY state has: #{Dict.get(cities, 'NY')}"
+puts "OR state has: #{Dict.get(cities, 'OR')}"
+
+#puts some states
+puts '-' * 10
+puts "Michigan's abbreviation is: #{Dict.get(states, 'Michigan')}"
+puts "Florida's abbreviation is: #{Dict.get(states, 'Florida')}"
+
+# puts state by using state then cities dic
+puts '-' * 10
+puts "Michigan has: #{Dict.get(cities, Dict.get(states, 'Michigan'))}"
+puts "Florida has: #{Dict.get(cities, Dict.get(states, 'Florida'))}"
+
+# puts every state abbreviation
+puts '-' * 10
+puts Dict.list(states)
+
+# puts every city in state
+puts '-' * 10
+Dist.list(cities)
+
+puts '-' * 10
+state = Dict.get(states, 'Texas')
+
+if !state # by default Ruby puts nil if something not there
+    puts "Sorry, no Texas."
+end
+
+city = Dist.get(cities, 'TX', 'Does not exist.') # default values using ||= with the nil result
+puts "The city for the state 'TX' is #{city}"
+
+
